e022b62e4d6e182a8c65be542aefc68d3eb57a8c
Adam MacDonald <am1macdonald@gmail.com>
2023-08-27 16:56:43 -0400

    Inital configuration

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{html,js,svelte,ts}'],
+  theme: {
+    extend: {},
+    fontFamily: {
+      display: ['"Bebas Neue"'],
+      body: ['Montserrat', 'Helvetica', 'Arial', 'sans-serif']
+    }
+  },
+  plugins: []
+};
